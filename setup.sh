4902520a4fce5326d9ec8cdc6c69a7d3f3586b95
Sean Heath <se@nheath.com>
2019-08-29 11:25:52 -0400

    added gdb extensions

--- setup.sh
+++ setup.sh
@@ -11,12 +11,13 @@ say()
 }
 
 me=$USER
-DIR="~/.config/linuxconfig/"
+HOME="/home/user"
+DIR="/home/user/.config/linuxconfig"
 
 say "Setting default make configuration"
-touch ~/.profile
-grep -qF -- "export MAKEFLAGS=" ~/.profile || echo "export MAKEFLAGS='-j$(nproc --all)'" >> ~/.profile
-source ~/.profile
+touch $HOME/.profile
+grep -qF -- "export MAKEFLAGS=" $HOME/.profile || echo "export MAKEFLAGS='-j$(nproc --all)'" >> $HOME/.profile
+source $HOME/.profile
 echo "Done"
 
 
@@ -36,97 +37,89 @@ sudo systemctl start libvirtd
 echo "Done"
 
 say "Installing VSCode"
-if [ -e "/usr/bin/code" ] ; then
-  echo "VSCode already installed"
-else
-  wget -O /tmp/vscode.deb "https://go.microsoft.com/fwlink/?LinkID=760868"
-  sudo apt install -y /tmp/vscode.deb
-  echo "Done"
-fi
+wget -O /tmp/vscode.deb "https://go.microsoft.com/fwlink/?LinkID=760868"
+sudo apt install -y /tmp/vscode.deb
+echo "Done"
 
 say "Installing NordVPN"
-if [ -e "/usr/bin/nordvpn" ] ; then
-  echo "NordVPN already installed"
-else
-  wget -O /tmp/nordvpn.deb "https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb"
-  sudo apt install -y /tmp/nordvpn.deb
-  sudo apt install -y nordvpn
-  echo "Done"
-fi
+wget -O /tmp/nordvpn.deb "https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb"
+sudo apt install -y /tmp/nordvpn.deb
+sudo apt install -y nordvpn
+echo "Done"
 
 say "Installing Keybase"
-if [ -e "/usr/bin/keybase" ] ; then
-  echo "Keybase is already installed"
-else
-  wget -O /tmp/keybase.deb https://prerelease.keybase.io/keybase_amd64.deb
-  sudo apt install -y /tmp/keybase.deb
-  echo "Done"
-fi
+wget -O /tmp/keybase.deb https://prerelease.keybase.io/keybase_amd64.deb
+sudo apt install -y /tmp/keybase.deb
+echo "Done"
 
 say "Installing Rust"
-if [ -e "/usr/local/bin/rustup" ]  ; then
-  echo "Rust already installed"
-else
-  sudo wget -O /usr/local/bin/rustup https://sh.rustup.rs
-  sudo chmod 0755 /usr/local/bin/rustup
-  rustup -y
-  echo "Done"
-fi
+wget -O /tmp/rust-init.sh https://sh.rustup.rs 
+chmod +x /tmp/rust-init.sh
+yes 1 | /tmp/rust-init.sh
+echo "Done"
 
 say "Setting up vim"
-if [ -d "$HOME/.vim/" ]; then
-  echo "Vim already installed"
-else
-  git clone https://github.com/seandheath/vim.git ~/.vim
-  cd ~/.vim
-  /bin/bash setup.sh
-  cd $DIR
-  echo "Done"
-fi
+git clone https://github.com/seandheath/vim.git $HOME/.vim
+cd $HOME/.vim
+/bin/bash setup.sh
+cd $DIR
+echo "Done"
 
 say "Setting up bash"
-if [ -d "$HOME/.bash/" ]; then
-  echo "Bash already set up"
-else
-  git clone https://github.com/seandheath/bash.git ~/.bash
-  cd ~/.bash
-  /bin/bash setup.sh
-  cd $DIR
-  echo "Done"
-fi
+git clone https://github.com/seandheath/bash.git $HOME/.bash
+cd $HOME/.bash
+/bin/bash setup.sh
+cd $DIR
+echo "Done"
 
 say "Setting up PEDA"
-if [ -d "/opt/peda/" ]; then
-  echo "PEDA already set up"
-else
-  sudo git clone https://github.com/longld/peda.git /opt/peda
-  echo "source /opt/peda/peda.py" >> ~/.gdbinit
-  echo "Done"
-fi
+mkdir --parents $HOME/.gdbmod/
+git clone https://github.com/longld/peda.git $HOME/.gdbmod/peda
+cd $DIR
+sudo cp files/gdb-peda /usr/bin/gdb-peda
+sudo chmod +x /usr/bin/gdb-peda
+echo "Done"
+
+say "Setting up Pwndbg"
+mkdir --parents $HOME/.gdbmod/
+git clone https://github.com/pwndbg/pwndbg $HOME/.gdbmod/pwndbg
+cd $HOME/.gdbmod/pwndbg/
+echo $(pwd)
+./setup.sh
+cd $DIR
+sudo cp files/gdb-pwndbg /usr/bin/gdb-pwndbg
+sudo chmod +x /usr/bin/gdb-pwndbg
+echo "Done"
+
+say "Setting up GEF"
+mkdir --parents $HOME/.gdbmod/gef
+wget -O ~/.gdbmod/gef/gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
+cd $DIR
+sudo cp files/gdb-gef /usr/bin/gdb-gef
+sudo chmod +x /usr/bin/gdb-gef
+echo "Done"
+
+say "Setting gdbinit"
+cd $DIR
+cp files/gdbinit $HOME/.gdbinit
+echo "Done"
 
 say "Fixing touchpad"
-if [ -e "/lib/systemd/system-sleep/touchpad" ]; then
-  echo "Touchpad already fixed"
-else
-  sudo cp $DIR/files/touchpad /lib/systemd/system-sleep/touchpad
-  sudo chmod 0755 /lib/systemd/system-sleep/touchpad
-  echo "Done"
-fi
+sudo cp $DIR/files/touchpad /lib/systemd/system-sleep/touchpad
+sudo chmod 0755 /lib/systemd/system-sleep/touchpad
+echo "Done"
 
 say "Disabling system beep"
-if [ -e "~/.xprofile" ]; then
-  grep -qxF 'xset -b' ~/.xprofile || echo 'xset -b' >> ~/.xprofile
-else 
-  touch ~/.xprofile
-  echo 'xset -b' >> ~/.xprofile
-fi
+touch $HOME/.xprofile
+grep -qxF 'xset -b' $HOME/.xprofile || echo 'xset -b' >> $HOME/.xprofile
 echo "Done"
 
 say "Cleaning up directories"
 targets=(Music Pictures Public Templates Videos)
 for target in $targets; do 
-  if [ -d "~/$target" ]; then
-    rm ~/$target
+  if [ -d "$HOME/$target" ]; then
+    rm $HOME/$target
   fi
 done
 echo "Done"
+echo ""
