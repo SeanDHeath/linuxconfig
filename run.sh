3f1acfd86147a51e96deaf24a8854b9649863336
Lucky Observer <plutonium@luckyobserver.com>
2020-01-11 13:28:02 -0500

    updated some kali stuff

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-PKG="ebtables progress neovim keepassxc htop go tree qemu virt-manager syncthing cmake openvpn keybase-gui base-devel yay"
-AUR_PKG="slack-desktop rslsync gotop-git"
-PIP_PKG="openpyn"
+PKG="dconf-cli seclists gobuster tor"
 
 CONF_DIR="$HOME/.config/linuxconfig"
 if ! cd $CONF_DIR ; then
@@ -11,38 +9,15 @@ if ! cd $CONF_DIR ; then
 fi
 
 # Install pacman packages
-sudo pacman --needed --noconfirm -S $PKG
-
-# Install AUR packages
-yay --needed --noconfirm -S $AUR_PKG
+sudo apt update
+sudo apt install -y $PKG
 
 # Install pip packages
 sudo pip install $PIP_PKG
 
-# Install Joplin
-wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash
-
 # Install gtile
 if [ ! -d "$HOME/.local/share/gnome-shell/extensions/gTile@vibou" ]; then
 	git clone https://github.com/gTile/gTile.git $HOME/.local/share/gnome-shell/extensions/gTile@vibou
-fi
-
-# Install vim config
-if [ ! -d "$HOME/.vim" ]; then
-	mkdir -p $HOME/.config/nvim
-	cp $HOME/.config/linuxconfig/files/init.vim $HOME/.config/nvim/
-	git clone https://seandheath@github.com/seandheath/vim.git $HOME/.vim
-	cd $HOME/.vim
-	$HOME/.vim/setup.sh
-	cd $CONF_DIR
-fi
-
-# Install bash config
-if [ ! -d "$HOME/.bash" ]; then
-	git clone https://seandheath@github.com/seandheath/bash.git $HOME/.bash
-	cd $HOME/.bash
-	$HOME/.bash/setup.sh
-	cd $CONF_DIR
 fi
 
 # Set up dconf bindings
@@ -51,10 +26,3 @@ dconf load /org/gnome/desktop/interface/ < files/org-gnome-desktop-interface.dco
 dconf load /org/gnome/desktop/wm/keybindings/ < files/org-gnome-desktop-wm-keybindings.dconf
 dconf load /org/gnome/settings-daemon/plugins/media-keys/ < files/org-gnome-settings-daemon-plugins-media-keys.dconf
 dconf load /org/gnome/shell/extensions/gtile/ < files/org-gnome-shell-extensions-gtile.dconf
-
-# Set up files folder (if present)
-if [ -d "$HOME/files" ]; then
-	cd $HOME/files/config
-	$HOME/files/config/setup.sh
-	cd $CONF_DIR
-fi
